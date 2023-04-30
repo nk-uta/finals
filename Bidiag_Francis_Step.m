8d792401a9a8cced5848f5e8389c12ae37f7595c
naveenk <kumaraswamy@apple.com>
2023-04-29 18:46:44 -0700

    second problem

--- Bidiag_Francis_Step.m
+++ Bidiag_Francis_Step.m
@@ -25,47 +25,6 @@ for i = 1:size(Bi, 1) - 1
 		end
 end	
 		
-%{		
-column2 = [Bi(1, 1)
-			Bi(2, 1)];
-G_hat = Givens_rotation( column2);
-Bi(1:2,:) = G_hat' * Bi(1:2,:);
-
-
-column3 = [Bi(1, 2)
-			Bi(1, 3)];
-G_tilde = Givens_rotation( column3);
-Bi(:, 2:3) = Bi(:, 2:3) * G_tilde;
-
-
-column4 = [Bi(2, 2)
-			Bi(3, 2)];
-G_hat = Givens_rotation( column4);
-Bi(2:3,:) = G_hat' * Bi(2:3,:);
-
-
-column5 = [Bi(2, 3)
-			Bi(2, 4)];
-G_tilde = Givens_rotation( column5);
-Bi(:, 3:4) = Bi(:, 3:4) * G_tilde;
-
-
-column6 = [Bi(3, 3)
-			Bi(4, 3)];
-G_hat = Givens_rotation( column6);
-Bi(3:4,:) = G_hat' * Bi(3:4,:);
-
-
-column7 = [Bi(3, 4)
-			Bi(3, 5)];
-G_tilde = Givens_rotation( column7);
-Bi(:, 4:5) = Bi(:, 4:5) * G_tilde;
-
-column8 = [Bi(4, 4)
-			Bi(5, 4)];
-G_hat = Givens_rotation( column8);
-Bi(4:5,:) = G_hat' * Bi(4:5,:);
-%}
 
 Bi;
 
